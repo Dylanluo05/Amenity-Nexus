62f9965598fbb56d43a710ff1532fad3eff723c5
Firestorm0986 <firestorm0986@gmail.com>
2022-09-30 20:59:54 -0700

    adding to main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-from flask import Flask, render_template, redirect, url_for, session, request
 # redirect and url_for can be used for redirecting users to the login page if they are attempting to access their user profile,
 # but they are not yet logged in.
-from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
+from flask import render_template, redirect, url_for, session, request
+
 
 # create a Flask instance
-app = Flask(__name__)
+# app = Flask(__name__)
 
-app.secret_key = 'service-NEXUS-a72387as349sjidla02'
+# app.secret_key = 'service-NEXUS-a72387as349sjidla02'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'service-nexus'
-app.config['MYSQL_PASSWORD'] = 'NEXUS-TEMP'
-app.config['MYSQL_DB'] = 'nexusdatabase'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'service-nexus'
+# app.config['MYSQL_PASSWORD'] = 'NEXUS-TEMP'
+# app.config['MYSQL_DB'] = 'nexusdatabase'
 
-mysql = MySQL(app)
+# mysql = MySQL(app)
 
 
 @app.route('/')
 def home():
     return render_template("/foundation/home.html")
+
 
 @app.route('/about-dylan/')
 def about_dylan():
@@ -51,16 +51,18 @@ def about_sohan():
 def about_kurtis():
     return render_template("about-kurtis.html")
 
+
 @app.route('/about-aryan/')
 def about_aryan():
     return render_template("about-aryan.html")
+
 
 @app.route('/service-search/')
 def service_search():
     return render_template("service-search.html")
 
 
-@app.route('/signin/', methods = ["GET", "POST"])
+@app.route('/signin/', methods=["GET", "POST"])
 def signin():
     msg = ""
     if request == "POST" and "email" in request.form and "password" in request.form:
@@ -76,7 +78,8 @@ def signin():
             return "Sign in was successful!"
         else:
             msg = "Incorrect email and/or password!"
-    return render_template("/account/signin.html", msg = msg)
+    return render_template("/account/signin.html", msg=msg)
+
 
 @app.route('/signout/')
 def signout():
@@ -85,7 +88,8 @@ def signout():
     session.pop("email", None)
     return redirect(url_for("signin"))
 
-@app.route('/signup/', methods = ["GET", "POST"])
+
+@app.route('/signup/', methods=["GET", "POST"])
 def signup():
     msg = ""
     if request.method == "POST" and "email" in request.form and "password" in request.form:
@@ -101,7 +105,8 @@ def signup():
             cursor.execute("INSERT INTO useraccounts VALUES (NULL, %s, %s,)", (fullname, password, email,))
             mysql.connection.commit()
             msg = "You have successfully created a new account!x"
-    return render_template("/account/signup.html", msg = msg)
+    return render_template("/account/signup.html", msg=msg)
+
 
 @app.route("/userprofile/")
 def userprofile():
@@ -109,21 +114,39 @@ def userprofile():
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute("SELECT * FROM useraccounts WHERE id = %s", (session["id"],))
         user_account = cursor.fetchone()
-        return render_template("/account/userprofile.html", user_account = user_account)
+        return render_template("/account/userprofile.html", user_account=user_account)
     return redirect(url_for("signin"))
+
 
 @app.route('/personalization/')
 def personalization():
     return render_template("personalization.html")
 
+
 @app.route('/crud-test/')
 def crudtest():
     return render_template("crud-test.html")
 
-#Service Base URLs
+
+# Service Base URLs
 @app.route('/kumon/')
 def kumon():
     return render_template("/servicebase/kumon.html")
+
+
+@app.route('/mathnasium/')
+def mathnasium():
+    return render_template("/servicebase/mathnasium.html")
+
+
+@app.route('/teqneeq-24-hour-gym/')
+def teqneeqgym():
+    return render_template("/servicebase/teqneeq-24-hour-gym.html")
+
+
+@app.route('/The Art of Dentistry/')
+def artofdentistry():
+    return render_template("/servicebase/The Art of Destistry.html")
 
 
 if __name__ == "__main__":
