4d3eae548d25576fd3988309af418a7edc0a7bbe
Dylan Luo <dragonfly.luo@gmail.com>
2022-10-01 17:10:51 -0700

    Successfully set up CRUD

--- account/app_crud.py
+++ account/app_crud.py
@@ -42,9 +42,7 @@ def crud_login():
         email = request.form.get("email")
         password = request.form.get("password")
         if login(email, password):       # zero index [0] used as email is a tuple
-            return render_template('index.html')
-
-    # if not logged in, show the login page
+            return render_template('/foundation/home.html')
     return render_template("login.html")
 
 
@@ -55,10 +53,8 @@ def crud_authorize():
         # validation should be in HTML
         user_name = request.form.get("user_name")
         email = request.form.get("email")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password1")
-        phone = request.form.get("phone")                    # password should be verified
-        if authorize(user_name, email, password1, phone):    # zero index [0] used as user_name and email are type tuple
+        password1 = request.form.get("password1")                 # password should be verified
+        if authorize(user_name, email, password1):    # zero index [0] used as user_name and email are type tuple
             return redirect(url_for('crud.crud_login'))
     # show the auth user page if the above fails for some reason
     return render_template("authorize.html")
@@ -67,7 +63,7 @@ def crud_authorize():
 @app_crud.route('/logout/', methods=["GET", "POST"])
 def crud_logout():
     logout_user()
-    return render_template("index.html")
+    return render_template("login.html")
 
 
 # CRUD create/add
@@ -78,8 +74,7 @@ def create():
         po = Users(
             request.form.get("name"),
             request.form.get("email"),
-            request.form.get("password"),
-            request.form.get("phone")
+            request.form.get("password")
         )
         po.create()
     return redirect(url_for('crud.crud'))
